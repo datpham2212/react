a0305d3ebee3d3887a43136180382d75cb917667
datpham2212 <phamtatdat2212@gmail.com>
2024-04-14 20:02:27 +0900

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,38 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NotFound from '@/pages/NotFound';
-import Home from '@/pages/Home';
+import { RouterProvider } from "react-router-dom";
+
+import useRegisterStore from "@/store/useRegisterStore";
+import LoadingPage from "@/pages/LoadingPage";
+import { RegistrationRoute } from "@/router/RegistrationRoute";
+
+import { useEffect } from "react";
+import { NavigationType } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
+  const {
+    _hasHydrated,
+    contract,
+    productSelection,
+    currentPath,
+    setCurrentPath,
+  } = useRegisterStore();
+
+  const { router } = RegistrationRoute({
+    contract,
+    currentPath,
+    productSelection,
+  });
+
+  useEffect(() => {
+    router.subscribe((state) => {
+      if (state.historyAction === NavigationType.Pop) {
+        setCurrentPath(state.location.pathname);
+      }
+    });
+  }, []);
+
+  if (!_hasHydrated) return <LoadingPage />;
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
